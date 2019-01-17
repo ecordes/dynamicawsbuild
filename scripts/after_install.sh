6fc05b89a66d8422b998e4e1adc739a86efbd379
Eva Cordes <ecordes@gyassa.com>
2019-01-17 09:13:06 -0600

    Updated install and stop scripts.

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
-# Set ownership for all folders
-chown -R ubuntu:ubuntu /home/ubuntu/dyn/
 
-cd /home/ubuntu/dyn/dynamicruntime
-git pull
+defaultInstallDir="/home/ubuntu"
+if [ -z $1 ]; then
+  installDir=$defaultInstallDir
+else
+  installDir=$1
+fi
+
+echo "The install directory is $installDir"
+
+if [ ! -d "$installDir/dyn" ]; then
+  mkdir -p $installDir/dyn
+fi
+
+# Set ownership for all folders
+chown -R ubuntu:ubuntu $installDir/dyn/
+
+# Get the source from the git repository
+cd $installDir/dyn
+if [ ! -d "$installDir/dyn/dynamicruntime" ]; then
+  echo "Creating dynamicruntime repository by cloning"
+  git clone https://github.com/sampwhite/dynamicruntime.git
+else
+  echo "Update dynamicruntime repository from git"
+  cd dynamicruntime
+  git pull
+fi
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-pid=`cat /home/ubuntu/dyn/dynapp.pid`
 
-kill -9 $pid
+if [ -f "/home/ubuntu/dyn/dynapp.pid" ]; then
+  pid=`cat /home/ubuntu/dyn/dynapp.pid`
+fi
+
+if [ ! -z $pid ]; then
+  echo "Stopping server"
+  if kill -9 $pid; then
+    echo "Unable to kill process $pid"
+  fi
+  exit 0
+fi
+
+exit 0
