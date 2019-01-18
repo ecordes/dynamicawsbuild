745cede3779fbfe6416397e903435ecf25ece8a3
Eva Cordes <ecordes@gyassa.com>
2019-01-18 14:10:18 -0600

    Changed scripts to use new image.

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-cd /home/ubuntu/dyn
-./gradlew execute > server.out 2>&1 &
-echo $! > /home/ubuntu/dyn/dynapp.pid
+~/bin/startdn
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,15 +1,2 @@
 #!/bin/bash
-
-if [ -f "/home/ubuntu/dyn/dynapp.pid" ]; then
-  pid=`cat /home/ubuntu/dyn/dynapp.pid`
-fi
-
-if [ ! -z $pid ]; then
-  echo "Stopping server"
-  if kill -9 $pid; then
-    echo "Unable to kill process $pid"
-  fi
-  exit 0
-fi
-
-exit 0
+~/bin/stopdn
