fa027a8c49d1cbe6a494962d87ab0dcf8ac19d55
Eva Cordes <ecordes@gyassa.com>
2019-01-23 14:01:58 -0600

    temp change to test failing scripts.

--- scripts/validate_server.sh
+++ scripts/validate_server.sh
@@ -3,7 +3,8 @@
 while true
 do
   STATUS=$(curl -s -o /dev/null -w '%{http_code}' http://localhost:7070/health/info | head -n 1 | grep 200)
-
+ 
+  sleep 20
   if [ $STATUS -eq 200 ]; then
     echo "DynamicRuntime Server is running."
     break
